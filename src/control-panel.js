86a7858700713e62a8be35016c7fc8a4c2e7290e
Romain <rmtestard@gmail.com>
2018-11-30 12:11:06 +0100

    implementing flux store

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,6 +1,15 @@
-import { Dispatcher } from './flux';
+import { Dispatcher, Store } from './flux';
 
 const controlPanelDispatcher = new Dispatcher();
+
+const UPDATE_USERNAME = 'UPDATE_USERNAME';
+
+const userNameUpdateAction = name => {
+  return {
+    type: 'UPDATE_USERNAME',
+    value: name
+  };
+};
 
 document
   .getElementById(`userNameInput`)
@@ -11,9 +20,31 @@ document
   });
 
 document.forms.fontSizeForm.fontSize.forEach(element => {
-  element.addEventListener(`change`, ({ ttarget }) => {
+  element.addEventListener(`change`, ({ target }) => {
     controlPanelDispatcher.dispatch('TODO_FONTUPDATEACTION');
   });
+});
+
+class UserPrefsStore extends Store {
+  getInitialState() {
+    return {
+      userName: 'Romain',
+      fontSize: 'small'
+    };
+  }
+  __onDispatch(action) {
+    console.log('Store received dispatch', action);
+    this.__emitChange();
+  }
+  getUserPreferences() {
+    return this.__state;
+  }
+}
+
+const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
+
+userPrefsStore.addListener(state => {
+  console.info(`The current state is...`, state);
 });
 
 controlPanelDispatcher.register(action => {
