5f334b50fbb707949a8763cd4c62e6352f58e0fd
Romain <rmtestard@gmail.com>
2018-11-30 13:30:56 +0100

    rendering the application

--- src/tasks.js
+++ src/tasks.js
@@ -3,6 +3,32 @@ import { Dispatcher, ReduceStore } from './flux';
 
 const tasksDispatcher = new Dispatcher();
 
+const CREATE_TASK = `CREATE_TASK`;
+const COMPLETE_TASK = `COMPLETE_TASK`;
+const SHOW_TASKS = `SHOW_TASKS`;
+
+const createNewTaskAction = content => {
+  return {
+    type: CREATE_TASK,
+    value: content
+  };
+};
+
+const showTasksAction = show => {
+  return {
+    type: SHOW_TASKS,
+    value: show
+  };
+};
+
+const completeTaskAction = (id, isComplete) => {
+  return {
+    type: COMPLETE_TASK,
+    id,
+    value: isComplete
+  };
+};
+
 class TasksStore extends ReduceStore {
   getInitialState() {
     return {
@@ -33,6 +59,19 @@ class TasksStore extends ReduceStore {
   }
   reduce(state, action) {
     console.log('Reducing...', state, action);
+    let newState;
+    switch (action.type) {
+      case CREATE_TASK:
+        newState = {
+          ...state,
+          tasks: [...state.tasks]
+        };
+        newState.tasks.push({
+          id: id(),
+          content: action.value,
+          complete: false
+        });
+    }
     return state;
   }
   getState() {
@@ -40,6 +79,42 @@ class TasksStore extends ReduceStore {
   }
 }
 
-const taskStore = new TasksStore(tasksDispatcher);
+const TaskComponent = ({ content, complete, id }) =>
+  `<section>
+    ${content} <input type="checkbox" name="taskCompleteCheck" data-taskid=${id} ${
+    complete ? 'checked' : ''
+  }>
+  </section>
+  `;
+
+const render = () => {
+  const tasksSection = document.getElementById('tasks');
+  const state = tasksStore.getState();
+  const rendered = state.tasks
+    .filter(task => (state.showComplete ? true : !task.complete))
+    .map(TaskComponent)
+    .join('');
+  tasksSection.innerHTML = rendered;
+};
+
+document.forms.newTask.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = e.target.newTaskName.value;
+  if (name) {
+    tasksDispatcher.dispatch(createNewTaskAction(name));
+    e.target.newTaskName.value = null;
+  }
+});
+
+document
+  .getElementById(`showComplete`)
+  .addEventListener('change', ({ target }) => {
+    const showComplete = target.checked;
+    tasksDispatcher.dispatch(showTasksAction(showComplete));
+  });
+
+const tasksStore = new TasksStore(tasksDispatcher);
 
 tasksDispatcher.dispatch('TEST_DISPATCH');
+
+render();
